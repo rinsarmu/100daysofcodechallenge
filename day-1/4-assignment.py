20bbe94b6ec95b1b7469344fcc2742e4ab09241b
Robera Insarmu <roberaensermu@gmail.com>
2024-03-13 23:13:43 +0300

    band name generator

---
+++ day-1/4-assignment.py
@@ -0,0 +1,6 @@
+#Create the code that generats band name from pets name and city by accepting from the user.
+
+print('Welcome to the Band Name Generator.')
+city = input("What's name of the city you grew up in? \n")
+petName = input("What's your pets name? \n")
+print("Your band name could be "+ city + " "+ petName)
