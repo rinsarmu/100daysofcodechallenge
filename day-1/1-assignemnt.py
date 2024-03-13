c4cad63fff889e21d396118b6b3dfb97dbcadd97
Robera Insarmu <roberaensermu@gmail.com>
2024-03-13 10:08:24 +0300

    THe first commit

---
+++ day-1/1-assignemnt.py
@@ -0,0 +1,10 @@
+#print the function  that looks like this
+#Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+# ANswer
+print('Day 1 - Python Print Function')
+print('he function is declared like this:')
+
+print('print(\'what to print\')')
---
+++ day-1/main.py
@@ -0,0 +1,7 @@
+# used for commenting
+
+# print is the method we use to print to the console like conosle log on JS
+# so the print is an instruction that prints piece of texts inside the brackets
+# double quotes represent start and end of the string.
+print("Hello world")
+
