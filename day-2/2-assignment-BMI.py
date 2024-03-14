577eeeecc565bede6c62019aae03f2a62d2cb32b
Robera Insarmu <roberaensermu@gmail.com>
2024-03-14 10:31:16 +0300

    BMI calculation suing python/bin/python3

---
+++ day-2/2-assignment-BMI.py
@@ -0,0 +1,8 @@
+name = input('Enter Name of the Person: ')
+
+height = float(input('please your height: ')) 
+weight = float(input('please your weight: ')
+)
+bmi = int( weight / height ** 2)
+
+print("BMI of"+ name + " is: "+ str(bmi))
