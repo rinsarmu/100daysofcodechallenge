c9774d73133fcce8ea671f54817a40d14736551b
Robera Insarmu <roberaensermu@gmail.com>
2024-03-14 10:33:20 +0300

    BMI calculation

---
+++ day-2/1-assignment.py
@@ -0,0 +1,4 @@
+two_digits = input("Please write two digit number ")
+newStr = str(two_digits)
+
+print(int(newStr[0])+ int(newStr))
--- day-2/2-assignment-BMI.py
+++ day-2/2-assignment-BMI.py
@@ -1,8 +1,7 @@
 name = input('Enter Name of the Person: ')
+height = float(input('please '+ name+ ' height: ')) 
+weight = float(input('please '+ name+ ' weight: '))
 
-height = float(input('please your height: ')) 
-weight = float(input('please your weight: ')
-)
 bmi = int( weight / height ** 2)
 
-print("BMI of"+ name + " is: "+ str(bmi))
+print("BMI of "+ name + " is: "+ str(bmi))
